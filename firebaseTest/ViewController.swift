3b40826a606b98ee039294ec3a59208144d33cc3
kongbaguni <kongbaguni@gmail.com>
2020-03-02 14:56:51 +0900

    Initial Commit

---
+++ firebaseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebaseTest
+//
+//  Created by Changyul Seo on 2020/03/02.
+//  Copyright © 2020 Changyul Seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
